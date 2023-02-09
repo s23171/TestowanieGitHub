9d089c7a27fc22655007050b24d39c1190167423
„Dominika <s23171@pjwstk.edu.pl>
2023-02-09 15:01:00 +0100

    printHelloWorldAdded

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        printHelloWorld();
+    }
+    public static void printHelloWorld(){
+        System.out.println("HelloWorld");
     }
 }
